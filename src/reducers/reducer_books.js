db6e727d1a3f1d74f8b0e40e0725e22aad1d56d9
cperceful <cpercef1@gmail.com>
2018-04-03 16:26:47 -0500

    book-details renders based on action

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title : 'The Big Book Of Butts' },
-        {title : 'Chicken Soup For The Souless' },
-        {title : 'Idiots Guide To The Bat Realm' },
-        {title : 'The Bibble' },
+        {title : 'Die Hard', pages: 458 },
+        {title : 'Die Hard 2: Die Harder', pages: 58 },
+        {title : 'Die Hard With A Vengeance', pages: 548 },
+        {title : 'Live Free Or Die Hard', pages: 2 },
     ];
 }

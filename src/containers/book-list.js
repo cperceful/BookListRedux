45fa9ae46d8cba8cf85290452c48dd2e34746d4a
cperceful <cpercef1@gmail.com>
2018-04-03 14:38:42 -0500

    Books reduce and book-list container built

---
+++ src/containers/book-list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title} className="list-group-item">{book.title}</li>
+            );
+        });
+    }
+}
+
+function mapStateToProps(state) {
+    //Whatever is returned will show up as props inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title : 'The Big Book Of Butts' },
+        {title : 'Chicken Soup For The Souless' },
+        {title : 'Idiots Guide To The Bat Realm' },
+        {title : 'The Bibble' },
+    ];
+}
